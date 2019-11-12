90425f312582bebf80f713a01f8bfaac1d917cc5
agustin <agustin.danil@followoz.com>
2019-11-12 10:28:20 -0300

    Some fixes to Meal Adapter

--- app/src/main/java/com/example/mealme/ui/adapters/MealAdapter.kt
+++ app/src/main/java/com/example/mealme/ui/adapters/MealAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.mealme.ui.adapters
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,6 +11,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mealme.R
 import com.example.mealme.model.Meal
+import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.net.URL
+import kotlin.coroutines.CoroutineContext
 
 
 class MealAdapter(val mealList: ArrayList<Meal>) :
@@ -23,7 +32,6 @@ class MealAdapter(val mealList: ArrayList<Meal>) :
     override fun getItemCount(): Int = mealList.size
 
     override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
-        Log.e(TAG, "Binding ${mealList[position]}")
         holder.bind(mealList[position])
     }
 
@@ -32,34 +40,63 @@ class MealAdapter(val mealList: ArrayList<Meal>) :
      * ViewHolder
      */
     class MealViewHolder(inflater: LayoutInflater, val parent: ViewGroup) :
-        RecyclerView.ViewHolder(inflater.inflate(R.layout.meal_item, parent, false)) {
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.meal_item, parent, false)), CoroutineScope {
+
+        val TAG = this.javaClass.name
+
+//        private var job = Job()
+
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.Main
+        //+ job
+
 
         fun bind(meal: Meal) {
             val title = itemView.findViewById<TextView>(R.id.meal_item_title)
             val instructions = itemView.findViewById<TextView>(R.id.meal_item_instructions)
             val image = itemView.findViewById<ImageView>(R.id.meal_item_image)
+
             title.text = meal.name
-            instructions.text = meal.instructions
-//            image.setImageBitmap(fetchImage(meal.thumbURL))
+            if (meal.instructions.length > 150)
+                instructions.text = meal.instructions.take(150).plus("...")
+            else
+                instructions.text = meal.instructions
+
+            launch {
+                fetchImage(image, meal.id, meal.thumbURL)
+            }
         }
 
-//        suspend fun fetchImage(url: String) : Bitmap? {
-//            var bitmap: Bitmap? = null
-//
-//            withContext(Dispatchers.IO) {
-//                try {
-//                    val inputStream = URL(url).openStream()       // Download Image from URL
-//                    bitmap = BitmapFactory.decodeStream(inputStream)    // Decode Bitmap
-//
-//                    inputStream.close()
-//                } catch (e: Exception) {
-//                    Log.e(">>>>>>>", "Exception 1, Something went wrong!")
-//                    e.printStackTrace()
-//                }
-//
-//            }
-//
-//            return bitmap
-//        }
+        suspend fun fetchImage(image: ImageView, imageID: Int, url: String) {
+            var bitmap: Bitmap? = null
+
+            // Create image name to local store
+            val filename = imageID.toString() + url.takeLastWhile { it != '/' }
+            val file = File(image.context.filesDir, filename)
+
+            withContext(Dispatchers.IO) {
+                try {
+                    if (file.exists()) {
+                        val inputStream = FileInputStream(file)
+                        bitmap = BitmapFactory.decodeStream(inputStream)
+                        inputStream.close()
+                    } else {
+                        val inputStream = URL(url).openStream()
+                        bitmap = BitmapFactory.decodeStream(inputStream)
+                        val outputStrem = FileOutputStream(file)
+                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, outputStrem)
+                        outputStrem.apply {
+                            flush()
+                            close()
+                        }
+                        inputStream.close()
+                    }
+                    image.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    Log.e(">>>>>>>", "Exception 1, Something went wrong!")
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mealme/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mealme/ui/main/MainFragment.kt
@@ -4,15 +4,17 @@ import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mealme.R
 import com.example.mealme.model.Meal
 import com.example.mealme.ui.adapters.MealAdapter
 import kotlinx.android.synthetic.main.main_fragment.*
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.opengl.Visibility
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+
 
 class MainFragment : Fragment() {
 
@@ -55,8 +57,11 @@ class MainFragment : Fragment() {
 
     private fun setObservers() {
         viewModel.getSearchResult().observe(this, Observer<ArrayList<Meal>> { meals ->
+            fmain_progress.visibility = View.INVISIBLE
+            fmain_recyclerview.visibility = View.VISIBLE
+
             mealsList.clear()
-            mealsList.addAll(meals)
+            mealsList.addAll(meals ?: ArrayList())
             mealAdapter.notifyDataSetChanged()
         })
     }
@@ -65,9 +70,25 @@ class MainFragment : Fragment() {
     private fun setButtonsListeners() {
         // Search button
         fmain_btn_search.setOnClickListener {
-            val searchedText = fmain_searched_text.text.toString()
+            fmain_progress.visibility = View.VISIBLE
+            fmain_recyclerview.visibility = View.INVISIBLE
 
+            val searchedText = fmain_searched_text.text.toString()
             viewModel.searchMeals(searchedText)
+
+            // Hide keyboard
+            try {
+                val imm = activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+                imm!!.hideSoftInputFromWindow(activity?.currentFocus?.getWindowToken(), 0)
+            } catch (e: Exception) { }
+        }
+
+        // On press enter
+        fmain_searched_text.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                fmain_btn_search.callOnClick()
+            }
+            false
         }
     }
 
